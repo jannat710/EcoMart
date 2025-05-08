2db0b10cfecf88d914f25fab147a2d672cecd2ed
jannat710 <jannatul0040@gmail.com>
2025-05-08 22:49:29 +0600

    fixed issues

--- ecomart-server/src/app.ts
+++ ecomart-server/src/app.ts
@@ -15,7 +15,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'http://localhost:5175',
+      'https://ecomart-server.vercel.app',
       'http://localhost:3000',
     ],
     credentials: true,
